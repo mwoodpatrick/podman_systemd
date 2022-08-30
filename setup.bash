0ea1f89005868d207a5e17d7ba99e0dab53cb6e1
mwoodpatrick <mwoodpatrick@gmail.com>
2022-08-30 16:39:31 -0700

    readd nginix

--- setup.bash
+++ setup.bash
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
-export PROJECT_DIR=$0
+export PROJECT_DIR=$(pwd)
+echo PROJECT_DIR=$PROJECT_DIR
 export XDG_CONFIG_HOME=${PROJECT_DIR}/config
 export XDG_DATA_HOME=${PROJECT_DIR}/data
