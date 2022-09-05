d154684430ed108194f934bad117a9183c10cd80
mwoodpatrick <mwoodpatrick@gmail.com>
2022-09-05 14:06:44 +0000

    fix some execution issues

--- nginx-unit/php/simple/runme.bash
+++ nginx-unit/php/simple/runme.bash
@@ -1,7 +1,6 @@
 #! /usr/bin/env bash
-cp -pr blogs /www/blogs
-curl -X PUT --data-binary @app_config.json --unix-socket /var/run/unit/control.sock http://localhost/config/applications/blogs
-curl -X PUT --data-binary @listner_config.json --unix-socket   /var/run/unit/control.sock http://localhost/config/listeners/127.0.0.1:8300
+sudo curl -X PUT --data-binary @app_config.json --unix-socket /var/run/unit/control.sock http://localhost/config/applications/blogs
+sudo curl -X PUT --data-binary @listner_config.json --unix-socket   /var/run/unit/control.sock http://localhost/config/listeners/127.0.0.1:8300
 sudo curl --unix-socket  /var/run/unit/control.sock http://localhost/config/
 curl http://127.0.0.1:8300
 curl http://127.0.0.1:8300/fred.php
