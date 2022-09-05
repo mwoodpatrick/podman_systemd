c08ff48b5d1992ee7b87620994bf4741c7a16fd4
mwoodpatrick <mwoodpatrick@gmail.com>
2022-09-05 20:44:49 +0000

    fix simple php test case

--- nginx-unit/php/simple/runme.bash
+++ nginx-unit/php/simple/runme.bash
@@ -1,8 +1,8 @@
 #! /usr/bin/env bash
 sudo curl -X DELETE --unix-socket /var/run/unit/control.sock http://localhost/config
-sudo curl -X PUT --data-binary @app_config.json --unix-socket /var/run/unit/control.sock http://localhost/config/applications/blogs
-sudo curl -X PUT --data-binary @listner_config.json --unix-socket   /var/run/unit/control.sock http://localhost/config/listeners/127.0.0.1:8300
+sudo curl -X PUT --data-binary @app_config.json --unix-socket /var/run/unit/control.sock http://localhost/config
+# sudo curl -X PUT --data-binary @listner_config.json --unix-socket   /var/run/unit/control.sock http://localhost/config/listeners/127.0.0.1:8300
 sudo curl --unix-socket  /var/run/unit/control.sock http://localhost/config/
-curl http://127.0.0.1:8300
-curl http://127.0.0.1:8300/fred.php
-curl http://127.0.0.1:8300/phpinfo.php
+curl http://127.0.0.1/phpinfo.php
+curl http://127.0.0.1
+curl http://127.0.0.1/fred.php
